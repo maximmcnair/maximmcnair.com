ed5c115f67286473a0c2fa34ebdc13fa45182ec7
Maxim McNair <mail@maximmcnair.com>
2023-02-23 14:28:15 +0000

    add goto link to "windows"

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -23,6 +23,11 @@ const DragComp: React.FC<Props> = ({ children, x: defaultX, y: defaultY, title, 
                 <a href={href} target="_blank" rel="noreferrer" className={styles.link}>{title}</a>:
                 <span>{title}</span>
               }
+              <a href={href} target="_blank" rel="noreferrer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={styles.icon}>
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                </svg>
+              </a>
             </header>
             {children}
           </div>
@@ -39,12 +44,17 @@ const DragComp: React.FC<Props> = ({ children, x: defaultX, y: defaultY, title, 
               <a href={href} target="_blank" rel="noreferrer" className={styles.link}>{title}</a>:
               <span>{title}</span>
             }
+            <a href={href} target="_blank" rel="noreferrer">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={styles.icon}>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+              </svg>
+            </a>
           </header>
           {children}
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default  DragComp;
